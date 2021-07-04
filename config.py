bb84895835b14d2eccf9b0d8fc2cd072f1547a98
fadilniju <76585854+fadilniju@users.noreply.github.com>
2021-07-04 08:36:15 +0530

    Update config.py
    Use you own Bot Token , API ID , HASH

--- config.py
+++ config.py
@@ -10,9 +10,9 @@ def init_log() -> int:
 
 
 class Config:
-    BOT_TOKEN = os.environ["BOT_TOKEN"]
-    API_ID = int(os.environ["API_ID"])
-    API_HASH = os.environ["API_HASH"]
+    BOT_TOKEN = os.environ["1854010658:AAG7mV47VuzxCu0QS7d_JOgsIOF7I_qja1o"]
+    API_ID = int(os.environ["6587260"])
+    API_HASH = os.environ["c2ca5eb4d7fe8ad25cb7a58905efa20f"]
     EXEC_PATH = os.environ.get("GOOGLE_CHROME_SHIM", None)
     # OPTIONAL
     LOG_GROUP = init_log()
